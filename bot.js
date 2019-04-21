0354c6154d13e07c6cc7581e0cb0bc0987620e5e
Annica321 <cafaroserena@virgilio.it>
2019-04-21 13:08:10 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Ready for dedth!');
+});
+
+client.on('message', msg => { 
+
+  var command = msg.content.toLowerCase()
+  var answer = ["yes kero!", "no kero!", "maybe kero!", "I can't say kero!", "I don't know kero!", "I'm too hungry to reply kero!"];
+
+      if (command.includes( ['hey keppi i love you'] )){
+        msg.react('💚');
+        }
+
+        if (command.includes( ['hey keppi'] )){
+          msg.reply('Kero!');
+          }
+
+  else if ( command.startsWith(['hey'])  && command.endsWith(['?']) ){
+      msg.reply(answer[Math.floor(Math.random()*answer.length)]);
+      }
+  });
+      bot.login(process.env.BOT_TOKEN);
